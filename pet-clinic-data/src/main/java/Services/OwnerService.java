43310cf595a1aa90eb50889e23a49a13236567d2
moqadar <moqadar.atifmal@gmail.com>
2021-10-14 23:26:23 -0400

    service layer

---
+++ pet-clinic-data/src/main/java/Services/OwnerService.java
@@ -0,0 +1,14 @@
+package Services;
+
+import springpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findByLastName(String lastName);
+    Owner findbyId(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+
+
+}
---
+++ pet-clinic-data/src/main/java/Services/VetService.java
@@ -0,0 +1,12 @@
+package Services;
+
+import springpetclinic.model.Pet;
+import springpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
